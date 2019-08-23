b7c53c47a4bef217d2cb1a0d140fffdef69b9439
Isuru Fernando <isuruf@gmail.com>
2019-08-23 13:00:34 -0500

    Use -MT for now

--- recipe/conda_build_wrapper.sh
+++ recipe/conda_build_wrapper.sh
@@ -9,10 +9,10 @@ export AS=llvm-as
 export AR=llvm-ar
 export NM=llvm-nm
 export LD=lld-link
-export CFLAGS="-I$PREFIX/include -O2 -D_CRT_SECURE_NO_WARNINGS -D_DLL -D_MT"
+export CFLAGS="-I$PREFIX/include -O2 -D_CRT_SECURE_NO_WARNINGS -D_MT"
 export CXXFLAGS="$CFLAGS"
 export CPPFLAGS="$CFLAGS"
-export LDFLAGS="-L$PREFIX/lib -Wl,-nodefaultlib:libcmt.lib -Wl,-defaultlib:msvcrt.lib"
+export LDFLAGS="-L$PREFIX/lib -fuse-ld=lld"
 export lt_cv_deplibs_check_method=pass_all
 
 echo "You need to run patch_libtool bash function after configure to fix the libtool script."
@@ -27,6 +27,7 @@ patch_libtool () {
     echo "#!/bin/bash" > libtool
     echo "export_symbols_cmds=\"$SRC_DIR/create_def.sh \\\$export_symbols \\\$libobjs \\\$convenience \"" >> libtool
     echo "archive_expsym_cmds=\"\\\$CC -o \\\$tool_output_objdir\\\$soname \\\$libobjs \\\$compiler_flags \\\$deplibs -Wl,-DEF:\\\\\\\"\\\$export_symbols\\\\\\\" -Wl,-DLL,-IMPLIB:\\\\\\\"\\\$tool_output_objdir\\\$libname.dll.lib\\\\\\\"; echo \"" >> libtool
+    sed -i.bak "s@|-fuse@|-fuse-ld=*|-fuse@g"
     cat libtool2 >> libtool
 }
 
