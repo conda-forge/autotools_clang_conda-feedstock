4d6ac64bd5c5ee299b206d94ac3e1842b701b335
H. Vetinari <h.vetinari@gmx.com>
2022-04-01 16:47:06 +1100

    fix tests

--- recipe/run_autotools_clang_conda_build.bat
+++ recipe/run_autotools_clang_conda_build.bat
@@ -9,9 +9,17 @@ copy "%LIBRARY_BIN%\conda_build_wrapper.sh" .
 set MSYSTEM=MINGW%ARCH%
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
-FOR /F "delims=" %%i in ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "PREFIX=%%i"
+
+FOR /F "delims=" %%i in ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "NEW_PREFIX=%%i"
+:: fallback in case LIBRARY_PREFIX wasn't set (or resolvable) directly
+IF "%NEW_PREFIX%"=="/" (
+    FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i/Library"
+) else (
+    set "PREFIX=%NEW_PREFIX%"
+)
 FOR /F "delims=" %%i in ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "BUILD_PREFIX=%%i"
 FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
 FOR /F "delims=" %%i in ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
+
 bash -lce "./conda_build_wrapper.sh %BUILDSCRIPT%"
 if %ERRORLEVEL% neq 0 exit 1
--- recipe/test_build_subpackage-sh.bat
+++ recipe/test_build_subpackage-sh.bat
@@ -1,4 +1,5 @@
-:: in test environment, we don't have %BUILD_PREFIX% available like for
-:: the example in the description of meta.yaml; adapt accordingly
+:: in test environment, we don't have PREFIX/SRC_DIR set; work around it
+set "PREFIX=%BUILD_PREFIX%\..\_test_env"
+set "SRC_DIR=%BUILD_PREFIX%\..\test_tmp"
 call %LIBRARY_BIN%\run_autotools_clang_conda_build.bat build_subpackage.sh
 if %ERRORLEVEL% neq 0 exit 1
