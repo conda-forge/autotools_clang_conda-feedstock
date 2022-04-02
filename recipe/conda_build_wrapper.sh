20ede3c219a4793b87ff39ac89797053dca614cd
H. Vetinari <h.vetinari@gmx.com>
2022-04-03 07:56:05 +1100

    cd into containing folder to patch libtool

--- recipe/conda_build_wrapper.sh
+++ recipe/conda_build_wrapper.sh
@@ -24,6 +24,8 @@ patch_libtool () {
     # libtool has support for exporting symbols using either nm or dumpbin with some creative use of sed and awk,
     # but neither works correctly with C++ mangling schemes.
     # cmake's dll creation tool works, but need to hack libtool to get it working
+    cwd=$(pwd)
+    cd $PREFIX/bin
     sed -i.bak "s/export_symbols_cmds=/export_symbols_cmds2=/g" libtool
     sed "s/archive_expsym_cmds=/archive_expsym_cmds2=/g" libtool > libtool2
     echo "#!/bin/bash" > libtool
@@ -31,6 +33,7 @@ patch_libtool () {
     echo "archive_expsym_cmds=\"\\\$CC -o \\\$tool_output_objdir\\\$soname \\\$libobjs \\\$compiler_flags \\\$deplibs -Wl,-DEF:\\\\\\\"\\\$export_symbols\\\\\\\" -Wl,-DLL,-IMPLIB:\\\\\\\"\\\$tool_output_objdir\\\$libname.dll.lib\\\\\\\"; echo \"" >> libtool
     cat libtool2 >> libtool
     sed -i.bak "s@|-fuse@|-fuse-ld=*|-nostdlib|-fuse@g" libtool
+    cd $cwd
 }
 
 if [[ "${REMOVE_LIB_PREFIX}" != "no" ]]; then
